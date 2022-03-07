d2693e595dba6039eecbf3dffc14853b2ad5febf
nutta-git <59726608+nutta-git@users.noreply.github.com>
2022-03-07 16:29:29 -0500

    Update user.js
    update with arkenfox's build

--- user.js
+++ user.js
@@ -11,24 +11,25 @@ This script MAY casue unknown issues. Use at your own RISK!
 
 user_pref("browser.startup.page", 0);
 user_pref("browser.startup.homepage", "about:blank");
-user_pref("startup.homepage_welcome_url", "");
-user_pref("startup.homepage_welcome_url.additional", "");
-user_pref("startup.homepage_override_url", "");
-user_pref("browser.startup.homepage_override.mstone", "ignore")
 
 user_pref("app.update.auto", false);
 user_pref("app.update.background.scheduling.enabled", false);
 
+user_pref("browser.search.update", false);
+user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.urlbar.suggest.engines", false);
 user_pref("browser.urlbar.suggest.openpage", false);
 user_pref("browser.urlbar.suggest.topsites", false);
 user_pref("browser.urlbar.suggest.history", false);
 user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+
+user_pref("browser.formfill.enable", false);
 user_pref("browser.urlbar.trimURLs", false);
 user_pref("browser.urlbar.speculativeConnect.enabled", false);
 user_pref("browser.urlbar.dnsResolveSingleWordsAfterSearch", 0);
 
-user_pref("browser.newtabpage.pinned", "");
 user_pref("browser.newtab.preload", false);
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
@@ -43,30 +44,30 @@ user_pref("browser.newtabpage.activity-stream.default.sites", "");
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
 
-user_pref("browser.safebrowsing.passwords.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.safebrowsing.phishing.enabled", false);
 user_pref("browser.safebrowsing.downloads.enabled", false);
 user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 user_pref("browser.safebrowsing.downloads.remote.url", "");
-user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
-user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
 
 user_pref("browser.region.network.url", "");
 user_pref("browser.region.update.enabled", false);
-user_pref("browser.search.update", false);
-user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.uitour.enabled", false);
 user_pref("browser.uitour.url", "");
 user_pref("browser.fixup.alternate.enabled", false);
+
+user_pref("browser.discovery.enabled", false);
+user_pref("browser.places.speculativeConnect.enabled", false);
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.send_pings", false);
+user_pref("browser.ping-centre.telemetry", false);
 
 user_pref("app.normandy.api_url", "");
 user_pref("app.normandy.enabled", false);
 user_pref("app.normandy.startupRolloutPrefs.doh-rollout.enabled", false);
 
-user_pref("beacon.enabled", false);
-user_pref("browser.send_pings", false);
-user_pref("browser.ping-centre.telemetry", false);
 user_pref("captivedetect.canonicalURL", "");
 user_pref("app.shield.optoutstudies.enabled", false);
 user_pref("messaging-system.rsexperimentloader.enabled", false);
@@ -86,33 +87,16 @@ user_pref("toolkit.telemetry.coverage.opt-out", true);
 user_pref("toolkit.coverage.opt-out", true);
 user_pref("toolkit.coverage.endpoint.base", "");
 
-user_pref("extensions.getAddons.showPane", false);
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
-user_pref("extensions.systemAddon.update.enabled", false);
-user_pref("extensions.systemAddon.update.url", "");
 user_pref("extensions.webcompat-reporter.enabled", false);
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false)
 user_pref("extensions.pocket.enabled", false);
+user_pref("identity.fxaccounts.enabled", false);
 
 user_pref("network.prefetch-next", false);
 user_pref("network.dns.disablePrefetch", true);
+user_pref("network.predictor.enable-prefetch", false);
 user_pref("network.predictor.enabled", false);
 user_pref("network.http.speculative-parallel-limit", 0);
 user_pref("network.captive-portal-service.enabled", false);
 user_pref("network.connectivity-service.enabled", false);
-
-user_pref("security.ssl.errorReporting.automatic", false);
-user_pref("security.ssl.errorReporting.enabled", false);
-user_pref("security.ssl.errorReporting.url", "");
-
-user_pref("dom.push.serverURL", "");
-user_pref("dom.security.unexpected_system_load_telemetry_enabled", false);
-user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
-user_pref("dom.ipc.plugins.reportCrashURL", false);
-
-user_pref("browser.discovery.enabled", false);
-user_pref("breakpad.reportURL", "");
-user_pref("browser.tabs.crashReporting.sendReport", false);
-user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
-user_pref("identity.fxaccounts.enabled", false);
-
-***/
