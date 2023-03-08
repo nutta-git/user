1087b56d1646bd65732a2c82a3fc6d7bc39c71c2
duevo <59726608+nutta-git@users.noreply.github.com>
2023-03-08 12:52:53 -0500

    Update user.js

--- user.js
+++ user.js
@@ -13,8 +13,6 @@ user_pref("browser.startup.homepage_override.mstone", "ignore");
 user_pref("browser.startup.homepage", "about:blank");
 
 user_pref("browser.newtabpage.enabled", false);
-user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
-user_pref("browser.newtabpage.activity-stream.telemetry", false);
 user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
@@ -22,12 +20,8 @@ user_pref("browser.newtabpage.activity-stream.showSponsored", false);
 user_pref("browser.newtabpage.activity-stream.feeds.discoverystreamfeed", false);
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 user_pref("browser.newtabpage.activity-stream.default.sites", "");
-
-user_pref("browser.search.suggest.enabled", false);
-user_pref("browser.urlbar.suggest.searches", false);
 user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
 user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
-user_pref("browser.urlbar.dnsResolveSingleWordsAfterSearch", 0);
 
 user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.safebrowsing.phishing.enabled", false);
@@ -36,7 +30,6 @@ user_pref("browser.safebrowsing.downloads.remote.enabled", false);
 user_pref("browser.safebrowsing.downloads.remote.block_potentially_unwanted", false);
 user_pref("browser.safebrowsing.downloads.remote.block_uncommon", false);
 
-user_pref("extensions.getAddons.showPane", false);
 user_pref("extensions.pocket.enabled", false);
 user_pref("browser.discovery.enabled", false);
 user_pref("identity.fxaccounts.enabled", false);
@@ -58,24 +51,13 @@ user_pref("toolkit.telemetry.coverage.opt-out", true);
 user_pref("toolkit.coverage.opt-out", true);
 user_pref("toolkit.coverage.endpoint.base", "");
 
-user_pref("app.shield.optoutstudies.enabled", false);
-
-user_pref("app.normandy.enabled", false);
-user_pref("app.normandy.api_url", "");
-
 user_pref("breakpad.reportURL", "");
 user_pref("browser.tabs.crashReporting.sendReport", false);
-
-
-/***
-user_pref("network.prefetch-next", false);
-user_pref("network.dns.disablePrefetch", true);
-user_pref("network.predictor.enabled", false);
-user_pref("network.predictor.enable-prefetch", false);
-user_pref("network.http.speculative-parallel-limit", 0);
-user_pref("network.connectivity-service.enabled", false);
-user_pref("browser.places.speculativeConnect.enabled", false);
-user_pref("browser.newtab.preload", false);
 user_pref("browser.ping-centre.telemetry", false);
-user_pref("browser.urlbar.speculativeConnect.enabled", false);
-***/
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false)
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
